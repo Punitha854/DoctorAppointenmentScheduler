daf289d5a0872ba42ec898746d1c863f866e5290
Punitha854 <punisarguru@gmail.com>
2019-09-27 12:55:38 +0200

    Adding Migration Files

---
+++ DoctorSchedulerAPI/Migrations/20190924171250_specialization.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DoctorSchedulerAPI.Migrations
+{
+    public partial class specialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Qualification",
+                table: "Doctors",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Specialization",
+                table: "Doctors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Qualification",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "Specialization",
+                table: "Doctors");
+        }
+    }
+}
